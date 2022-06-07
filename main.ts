23c2994953d30770be1ff3fb0cded231dc13d6aa
David00023 <106170411+David00023@users.noreply.github.com>
2022-06-07 13:21:18 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+let min = 0
+let max = 0
+radio.setGroup(59)
+let currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
+basic.forever(function () {
+    if (currenttemperature > 5) {
+        radio.sendString("H")
+    }
+    basic.showString(".")
+    currenttemperature = input.temperature()
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
